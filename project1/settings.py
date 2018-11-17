ef358c748cde05ccde78f9456f3086c237a8f60a
YOUR_USERNAME <losman3331984@gmail.com>
2018-11-16 17:40:00 -0800

    asdf

--- project1/settings.py
+++ project1/settings.py
@@ -164,7 +164,6 @@ INSTALLED_APPS = (
                   'aldryn_apphooks_config',
                   'aldryn_categories',
                   'aldryn_common',
-                  'aldryn_newsblog',
                   'aldryn_people',
                   'aldryn_translation_tools',
                   'aldryn_gallery',
