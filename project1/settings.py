d533b4b780eabf16125bbd34a3ce9a52445b2784
YOUR_USERNAME <losman3331984@gmail.com>
2018-11-20 08:17:01 -0800

    asdf

--- project1/settings.py
+++ project1/settings.py
@@ -261,7 +261,7 @@ STATIC_URL = '/static/'
 STATICFILES_LOCATION = '/static/'
 
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
